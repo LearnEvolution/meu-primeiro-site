0b4db15126e3196ebb877e06cd3c64d83b5a6c8e
unknown <psh.evolutin@gmail.com>
2024-07-28 02:44:32 -0300

    adição de uma foto e texto

--- horasjs.js
+++ horasjs.js
@@ -1,25 +1,35 @@
 function iniciar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var top = window.document.getElementById('top')
     var data = new Date()
     var hora = data.getHours()
     var min = data.getMinutes()
   //  msg.innerHTML = `Agora são ${hora} horas`
     
-    if(hora >= 0 && hora < 12 ){
+    if(hora >= 6 && hora < 12 ){
        img.src = 'manha.png'
        document.body.style.background = '#51aff7'
-       msg.innerHTML = `Bom dia! Agora são ${hora}h ${min}m`
+       msg.innerHTML = `Agora são ${hora}h ${min}m`
+       top.innerHTML = 'Bom Dia!'
        
     }else if(hora >= 12 && hora <= 17){
        img.src = 'tarde.png'
-      document.body.style.background = '#ef8c4c'
-      msg.innerHTML = `Boa tarde! Agora são ${hora}h ${min}m`
-      
+      document.body.style.background = '#ef8c'
+      msg.innerHTML = `Agora são ${hora}h ${min}m`
+      top.innerHTML = 'Boa Tarde!'
+
+   }else if(hora >= 0 && hora <= 5){
+      img.src = 'madrugada.png'
+     document.body.style.background = '#000'
+     msg.innerHTML = `Agora são ${hora}h ${min}m`
+     top.innerHTML = 'Boa Madrugada!'
+
     }else{
        img.src = 'noite.png'
        document.body.style.background = '#3c3b3b'
-       msg.innerHTML = `Boa Noite! Agora são ${hora}h ${min}m`
+       msg.innerHTML = `Agora são ${hora}h ${min}m`
+       top.innerHTML = 'Boa Noite!'
   
     }
 }
